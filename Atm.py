e408a18fd809d3504d2c45fe217e676bb52583b2
Astha <70913929+asthacoder@users.noreply.github.com>
2021-08-22 23:26:47 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,71 @@
+class Atm:
+    def _init__(self,cardnumber,pin):
+    self.cardnumber = cardnumber
+    self pin = pin
+
+def balanceinquiry(self):
+ print("Your balance is: $96000")
+
+def cashwithdrawal(self, amount):
+ new_amount = 1000-amount
+print("You withdrawed: " + str(amount) + "Your remaining balance is: " + str(new_amount))
+
+def main():
+ name = input("Hello what is your name?")
+  print("Hello, " + name)
+cardnumber = input("Insert your card number: ")
+pin = input ("Enter your pin: ")
+new_user = Atm(cardnumber, pin)
+
+print("choose your activity")
+print("1. Balance Inquirt")
+print("2.Cash Withdrawal")
+activity = int(input("Enter activity choice: "))
+
+if (activity ==1):
+ new_user.balanceinquiry()
+elif (activity == 2): 
+  amount = int(input("Enter the amount: "))
+  new_user.cashwithdrawal(amount)
+
+def main():
+       name = input("Hello what is your name?")
+       print("Hello, " + name)
+       cardnumber = input("Insert your card number: ")
+       pin = input("Enter your pin")
+       new_user = Atm(cardnumber, pin)
+       
+       print("Choose your activity")
+       print("1. Balance Inquiry")
+       print("2. Cash Withdrawal")
+       activity = int(input("Enter activity choice: "))
+       
+       if (activity == 1):
+             new_user.balanceinquiry()
+       elif (activity ==2):
+           amount = int(input("Enter the amount: "))
+           new_user.cashwidthdrawal(amount)
+       else:
+            print("Enter a valid number")
+            
+            if __name__ == "__main__":
+                main()   
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+   
+           
